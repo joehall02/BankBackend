f72ecfde439fa1c03119ca10f7af1e7684bebc3e
Joe Hall <joehall0207@outlook.com>
2022-11-22 00:50:04 +0000

    added overdraft views and controller

---
+++ BankBackend/Controllers/OverdraftController.cs
@@ -0,0 +1,23 @@
+﻿using BankBackend.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BankBackend.Controllers
+{
+	public class OverdraftController : Controller
+	{
+		public IActionResult Index()
+		{
+            List<Customer> customers = new List<Customer>();
+
+            using (var db = new BankContext())
+            {
+                // gets all customers from database
+                customers = db.Customers.Where(c => c.Salary >= 30000).ToList();
+            }
+
+            TempData["customers"] = customers;
+
+            return View();
+		}
+	}
+}
--- BankBackend/Models/Customer.cs
+++ BankBackend/Models/Customer.cs
@@ -10,8 +10,8 @@ namespace BankBackend.Models
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Address { get; set; }
-        public string Age { get; set; }
-        public string Salary { get; set; }
+        public int Age { get; set; }
+        public int Salary { get; set; }
 
     }
 }
