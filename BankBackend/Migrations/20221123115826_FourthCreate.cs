117aae427e6cdae780046634173052844196e5d2
Joe Hall <joehall0207@outlook.com>
2022-11-25 11:22:30 +0000

    i

---
+++ BankBackend/Migrations/20221123115826_FourthCreate.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BankBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class FourthCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CustomerId",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "Overdraft",
+                table: "Customers");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CustomerId",
+                table: "Customers",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "Customers",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Overdraft",
+                table: "Customers",
+                type: "INTEGER",
+                nullable: true);
+        }
+    }
+}
