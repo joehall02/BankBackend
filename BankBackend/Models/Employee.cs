8024d6433f52653b283c259427c8126a01e6ce18
Luke Fosbrook <luke.fosbrook@outlook.com>
2022-11-25 11:33:11 +0000

    Employee class

---
+++ BankBackend/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BankBackend.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int id   { get; set; }
+        public string Name { get; set; }    
+        public string Password  { get; set; }   
+        public string AccountType { get; set; } 
+
+    }
+}
